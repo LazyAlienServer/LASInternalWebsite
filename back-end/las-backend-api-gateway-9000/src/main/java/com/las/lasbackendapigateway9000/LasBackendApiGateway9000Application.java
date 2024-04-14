32fcf9ff0a86f1bbf5591ce69147dcf6d0f21652
sun-yinuo <2192519304@qq.com>
2024-04-14 22:08:05 +0800

    migrate backend-gateway

---
+++ back-end/las-backend-api-gateway-9000/src/main/java/com/las/lasbackendapigateway9000/LasBackendApiGateway9000Application.java
@@ -0,0 +1,17 @@
+package com.las.lasbackendapigateway9000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * gateway
+ * @author martin
+ */
+@SpringBootApplication
+public class LasBackendApiGateway9000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LasBackendApiGateway9000Application.class, args);
+	}
+
+}
---
+++ back-end/las-backend-api-gateway-9000/src/test/java/com/las/lasbackendapigateway9000/LasBackendApiGateway9000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.las.lasbackendapigateway9000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LasBackendApiGateway9000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
